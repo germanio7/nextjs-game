8fe39128d5b3d8e3909090ea1ea717889b8d176e
German Escobar <german.e.escobar@gmail.com>
2023-09-04 16:26:18 -0300

    animation

--- src/app/page.js
+++ src/app/page.js
@@ -1,18 +1,45 @@
 "use client";
-import { BlurFilter } from "pixi.js";
-import { Stage, Container, Sprite, Text } from "@pixi/react";
-import { useMemo } from "react";
+import { Stage, Container, Sprite, useTick } from "@pixi/react";
+import React, { useReducer, useRef } from "react";
 
 export default function Home() {
-  const blurFilter = useMemo(() => new BlurFilter(4), []);
+  const reducer = (_, { data }) => data;
+
+  const Bunny = () => {
+    const [motion, update] = useReducer(reducer);
+    const iter = useRef(0);
+
+    useTick((delta) => {
+      const i = (iter.current += 0.05 * delta);
+
+      update({
+        type: 'update',
+        data: {
+          x: Math.sin(i) * 100,
+          y: Math.sin(i / 1.5) * 100,
+          rotation: Math.sin(i) * Math.PI,
+          anchor: Math.sin(i / 2),
+        },
+      });
+    });
+
+    return <Sprite image="https://pixijs.io/pixi-react/img/bunny.png" {...motion} />;
+  };
 
   return (
-    <Stage>
-      <Sprite image="https://pixijs.io/pixi-react/img/bunny.png" x={400} y={270} anchor={{ x: 0.5, y: 0.5 }} />
-
-      <Container x={400} y={330}>
-        <Text text="Hello World" anchor={{ x: 0.5, y: 0.5 }} filters={[blurFilter]} />
-      </Container>
-    </Stage>
+    <div className="flex justify-center items-center">
+      <div className="flex-wrap justify-center items-center">
+        <div className="flex justify-center text-3xl">
+          Nextjs&nbsp;+&nbsp;<a className="underline" href="https://pixijs.io/pixi-react/" target="_blank">React Pixi</a>
+        </div>
+        <div className="flex justify-center">
+          <Stage width={300} height={300} options={{ backgroundAlpha: 0 }}>
+            <Container x={150} y={150}>
+              <Bunny />
+            </Container>
+          </Stage>
+        </div>
+      </div>
+    </div>
   );
 }
